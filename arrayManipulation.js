25867488e208443fe017cd25faea4f1da884089c
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-05-18 10:53:33 -0700

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,14 @@
-
+function processArray(numbers) {
+    return numbers.map(number => {
+      if (number % 2 === 0) {
+        return number * number;
+      } else {
+        return number * 3;
+      }
+    });
+  }
+  
+  const mynumbers = [15, 16, 17, 18, 19];
+  const processedNumbers = processArray(mynumbers);
+  console.log(processedNumbers); 
+  
